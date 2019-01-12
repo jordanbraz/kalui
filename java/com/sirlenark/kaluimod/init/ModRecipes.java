2da7e0ba971ed7205d36019cddc2823dddcf7885
jordanbraz <32969472+jordanbraz@users.noreply.github.com>
2019-01-12 10:14:57 -0200

    Add files via upload

--- java/com/sirlenark/kaluimod/init/ModRecipes.java
+++ java/com/sirlenark/kaluimod/init/ModRecipes.java
@@ -1,9 +1,13 @@
 package com.sirlenark.kaluimod.init;
 
 import com.sirlenark.kaluimod.Main;
+import com.sirlenark.kaluimod.items.blocks.BlockBase;
 import com.sirlenark.kaluimod.items.blocks.BlockOres;
 import com.sirlenark.kaluimod.util.handlers.EnumHandler;
+import com.sirlenark.kaluimod.world.gen.WorldGenCustomOres;
 
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.PropertyEnum;
 import net.minecraft.block.state.pattern.BlockMatcher;
 import net.minecraft.client.renderer.block.model.Variant;
@@ -17,7 +21,14 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 public class ModRecipes {	
 	
 	public static void init() {
-		GameRegistry.addSmelting(BlockRegister.ORE_OVERWORLD, new ItemStack(ItemRegister.RUBY,1), 1.0F);
+		//this work with ruby and sapphire, but i want only with ruby
+		GameRegistry.addSmelting(BlockRegister.ORE_OVERWORLD , new ItemStack(ItemRegister.RUBY,1), 1.0F);
+		
+		/*
+		just like this, but its not possible to find ore_overworld_ruby or sapphire
+		GameRegistry.addSmelting(BlockRegister.ORE_OVERWORLD_ruby , new ItemStack(ItemRegister.RUBY,1), 1.0F);
+		GameRegistry.addSmelting(BlockRegister.ORE_OVERWORLD_sapphire , new ItemStack(ItemRegister.Sapphire,1), 1.0F);
+		*/
 	}
 
 }
